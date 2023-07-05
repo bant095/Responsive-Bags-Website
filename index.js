f83901e9c3e9178ca0409e8643e343fbbfa0adbf
AyodejiAbolaji <abolajiayodeji93@gmail.com>
2023-07-05 03:37:47 +0100

    I added responsiveness

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 // Search
-
-
 let search = document.querySelector('.search-box');
 
 document.querySelector('#search-icon').onclick = () => {
     search.classList.toggle('active');
+    navbar.classList.remove('active');
 }
+
+// NavBar
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = () => {
+    navbar.classList.toggle('active');
+    search.classList.remove('active');
+}
+
+//Remove Menu And Search Icon on Scroll
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    search.classList.remove('active');
+}
+
+
+// Header Animation 
+
+let header = document.querySelector('header'); 
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('active', window.scrollY > 0);
+}); 
